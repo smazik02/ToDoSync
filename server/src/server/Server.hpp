861ec13e6a32a9ceae67174734fa46e310550dd6
smazik02 <s.mazik2002@gmail.com>
2024-12-18 18:31:45 +0100

    refactor: :recycle: changed Server Class declaration to implicit private

--- server/src/server/Server.hpp
+++ server/src/server/Server.hpp
@@ -9,6 +9,14 @@
 #include "../repository/Repository.hpp"
 
 class Server {
+    std::shared_ptr<Repository> repository_;
+    int socket_fd;
+    int epoll_fd;
+    sockaddr_in address{};
+    socklen_t address_len;
+    epoll_event events{};
+    std::map<int, std::unique_ptr<User> > users;
+
 public:
     Server(int port, std::shared_ptr<Repository> repository);
 
@@ -19,15 +27,6 @@ public:
     void stop();
 
     void terminate(const char *message);
-
-private:
-    std::shared_ptr<Repository> repository_;
-    int socket_fd;
-    int epoll_fd;
-    sockaddr_in address{};
-    socklen_t address_len;
-    epoll_event events{};
-    std::map<int, std::unique_ptr<User> > users;
 };
 
 
