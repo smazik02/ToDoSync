cf0955b980f47b7bd2093d68887296149debc7cf
Anna Gąsiorowska <80831299+Aeri4a@users.noreply.github.com>
2024-12-22 14:14:42 +0100

    TaskList operations

--- server/src/operation_service/OperationService.hpp
+++ server/src/operation_service/OperationService.hpp
@@ -23,14 +23,14 @@ public:
     ServiceResponse user_login(const nlohmann::json &payload, User *user) const;
 
     /**
-     * 
+     *
      * @param payload - { username: "username", task_list_name: "tasklist name" }
      * @return - { tasks: Task[] }
      */
     ServiceResponse get_all_tasks(const nlohmann::json &payload) const;
 
     /**
-     * 
+     *
      * @param payload - { username: "username", task_list_name: "tasklist name", task_name: "task name", task_description: "task description" }
      * @return - {}
      */
@@ -38,11 +38,29 @@ public:
 
 
     /**
-     * 
+     *
      * @param payload - { username: "username", task_id: "task id", task_list_name: "task list name" }
      * @return - {}
      */
     ServiceResponse remove_task(const nlohmann::json &payload) const;
+
+    /**
+    * @param username of connected and signed user
+    * @return { lists: ['<list_name1>', '<list_name2>'] }
+    */
+    ServiceResponse get_all_user_task_lists(const std::string &username) const;
+
+    /**
+    * @param payload - { name: <task_list_name> }
+    * @return status
+    */
+    ServiceResponse create_task_list(const nlohmann::json &payload, const std::string &username) const;
+
+    /**
+     * @param payload - { name: <task_list_name> }
+     * @return status
+     */
+    ServiceResponse join_task_list(const nlohmann::json &payload, const std::string &username) const;
 };
 
 
