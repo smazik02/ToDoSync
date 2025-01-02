f462b59f38c229d8198815d7930e0d2e73e3140d
smazik02 <s.mazik2002@gmail.com>
2025-01-02 14:44:42 +0100

    feat: :safety_vest: validation class to validate JSON payloads

---
+++ server/src/validator/Validator.cpp
@@ -0,0 +1,47 @@
+#include "Validator.hpp"
+
+#include "../exceptions.hpp"
+
+void Validator::validate(const nlohmann::json &input, const std::vector<ValidatorFieldData> &validator_data) {
+    std::string validator_string;
+
+    for (const auto &[field_name, field_type]: validator_data) {
+        if (!input.contains(field_name)) {
+            validator_string += "no " + field_name + " given, ";
+            continue;
+        }
+
+        bool correct;
+        switch (field_type) {
+            case BOOLEAN: {
+                correct = input.at(field_name).is_boolean();
+                break;
+            }
+            case INTEGER: {
+                correct = input.at(field_name).is_number_integer();
+                break;
+            }
+            case STRING: {
+                correct = input.at(field_name).is_string();
+                break;
+            }
+            default: correct = false;
+        }
+
+        if (!correct)
+            validator_string += "field " + field_name + " should be of type " + field_type_string(field_type) +
+                    ", ";
+    }
+
+    if (!validator_string.empty())
+        throw validator_error(validator_string.substr(0, validator_string.size() - 2));
+}
+
+const char *Validator::field_type_string(const FieldType field_type) {
+    switch (field_type) {
+        case BOOLEAN: return "bool";
+        case INTEGER: return "int";
+        case STRING: return "string";
+        default: return "?";
+    }
+}
---
+++ server/src/validator/Validator.hpp
@@ -0,0 +1,16 @@
+#ifndef VALIDATOR_HPP
+#define VALIDATOR_HPP
+
+#include <json.hpp>
+
+#include "../types.hpp"
+
+class Validator {
+public:
+    static void validate(const nlohmann::json &input, const std::vector<ValidatorFieldData> &validator_data);
+
+    static const char *field_type_string(FieldType field_type);
+};
+
+
+#endif //VALIDATOR_HPP
