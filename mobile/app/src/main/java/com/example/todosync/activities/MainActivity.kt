98abc8c2cdcafac346bd3aedef5b48d97ca5df67
smazik02 <s.mazik2002@gmail.com>
2025-01-04 21:30:34 +0100

    refactor: :bricks: changed mobile to Jetpack Compose

---
+++ mobile/app/src/main/java/com/example/todosync/activities/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.todosync.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todosync.ui.theme.ToDoSyncTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToDoSyncTheme {
+                Box(
+                    modifier = Modifier.windowInsetsPadding(WindowInsets.safeContent)
+                ) {
+                    OutlinedCard { Greeting("ToDoSync") }
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ToDoSyncTheme {
+        Greeting("Android")
+    }
+}
