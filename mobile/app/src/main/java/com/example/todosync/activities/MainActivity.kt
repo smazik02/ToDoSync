6979ef0b53099329db724935f3a9cdc2f632c187
smazik02 <s.mazik2002@gmail.com>
2025-01-10 20:12:19 +0100

    style: :lipstick: replaced Toast with SnackBar in Main Activity

--- mobile/app/src/main/java/com/example/todosync/activities/MainActivity.kt
+++ mobile/app/src/main/java/com/example/todosync/activities/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.todosync.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,23 +10,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todosync.ui.theme.ToDoSyncTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -35,64 +40,74 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val scope = rememberCoroutineScope()
+            val snackbarHostState = remember { SnackbarHostState() }
+
             var userNameText by remember { mutableStateOf("") }
 
             ToDoSyncTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .windowInsetsPadding(WindowInsets.safeContent)
+                Scaffold(
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
                     ) {
-                        Column(
+                        Box(
                             modifier = Modifier
-                                .align(Alignment.Center)
-                                .fillMaxWidth(),
-                            horizontalAlignment = Alignment.End
+                                .fillMaxSize()
+                                .windowInsetsPadding(WindowInsets.safeContent)
                         ) {
-                            OutlinedTextField(
-                                value = userNameText,
-                                onValueChange = { userNameText = it },
+                            Column(
                                 modifier = Modifier
-                                    .fillMaxWidth(),
-                                textStyle = MaterialTheme.typography.bodyLarge,
-                                label = {
+                                    .align(Alignment.Center)
+                                    .fillMaxWidth()
+                                    .padding(innerPadding),
+                                horizontalAlignment = Alignment.End
+                            ) {
+                                OutlinedTextField(
+                                    value = userNameText,
+                                    onValueChange = { userNameText = it },
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    textStyle = MaterialTheme.typography.bodyLarge,
+                                    label = {
+                                        Text(
+                                            text = "Username",
+                                            color = MaterialTheme.colorScheme.onSurface,
+                                            style = MaterialTheme.typography.bodyLarge
+                                        )
+                                    }
+                                )
+                                Button(
+                                    onClick = {
+                                        if (userNameText.isBlank()) {
+                                            scope.launch {
+                                                snackbarHostState.showSnackbar("Username cannot be blank")
+                                            }
+                                            return@Button
+                                        }
+                                        Intent(
+                                            applicationContext,
+                                            TaskListsActivity::class.java
+                                        ).also {
+                                            startActivity(it)
+                                        }
+                                    },
+                                    colors = ButtonDefaults.buttonColors(),
+                                ) {
                                     Text(
-                                        text = "Username",
-                                        color = MaterialTheme.colorScheme.onSurface,
-                                        style = MaterialTheme.typography.bodyLarge
+                                        text = "Login",
+                                        color = MaterialTheme.colorScheme.onPrimary,
+                                        style = MaterialTheme.typography.labelLarge,
+                                        maxLines = 1
                                     )
                                 }
-                            )
-                            Button(
-                                onClick = {
-                                    if (userNameText.isBlank()) {
-                                        Toast.makeText(
-                                            this@MainActivity,
-                                            "Username cannot be blank",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                        return@Button
-                                    }
-                                    Intent(applicationContext, TaskListsActivity::class.java).also {
-                                        startActivity(it)
-                                    }
-                                },
-                                colors = ButtonDefaults.buttonColors(),
-                            ) {
-                                Text(
-                                    text = "Login",
-                                    color = MaterialTheme.colorScheme.onPrimary,
-                                    style = MaterialTheme.typography.labelLarge
-                                )
                             }
                         }
                     }
                 }
-
             }
         }
     }
